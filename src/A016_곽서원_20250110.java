3d1fccd48e53805f01e24c9335059bece7fc64be
seowon <22100042@handong.ac.kr>
2025-01-10 16:58:41 +0900

    A016 Done

---
+++ src/A016_곽서원_20250110.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0; //어떻게 할거냐. 첫번째 반복문에서 limit - people[i]번째 사람의 몸무게
+        //두 번째 반복문에서 limit - people[i]>=people[j]
+        int sum=0;
+        int start=0;
+        int end=people.length-1;
+        Arrays.sort(people);
+        while(start<=end){
+
+            sum=people[start]+people[end];
+
+            if(sum<=limit){
+                start++;
+                end--;
+            }
+            else{
+                end--;
+            }
+            answer++;
+        }
+
+        return answer;
+
+
+    }
+}
