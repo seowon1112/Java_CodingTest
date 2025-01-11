6ef36e9155af0dad88e8de1ae1cf6b71b97b07ea
22100042 <22100042@handong.ac.kr>
2025-01-11 13:55:32 +0900

    A021 Done

---
+++ src/week2/A021_곽서원_20250111.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+
+            int sum=0;
+            for(int i=0; i<n; i++){
+
+                sum+=sc.nextInt();
+                if(i==0) continue;
+                sum--;
+
+            }
+            System.out.println(sum);
+    }
+}
